2251fcb946ab07510241ad609d5daa5eb4b9b306
vprada25 <v.prada@udla.edu.co>
2021-03-14 13:52:20 -0500

    feat(configuration): configuration strripe in index

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import {Elements} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 
 
-const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
+
 import ReactDOM from 'react-dom';
 import App from './App';
+
+const stripePromise = loadStripe('pk_test_JJ1eMdKN0Hp4UFJ6kWXWO4ix00jtXzq5XG');
 
 ReactDOM.render(
   <React.StrictMode>
